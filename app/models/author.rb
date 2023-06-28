31b8e303e0def087a263bb6a9fdc8df847271f0f
Koome <judekoome360@gmail.com>
2023-06-28 03:41:03 +0300

    COmplete

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,16 @@
 class Author < ApplicationRecord
-end
+    has_many :posts
+  
+    # add this:
+    has_one :profile
+  end
+  
+  class Profile < ApplicationRecord
+    # add this:
+    belongs_to :author
+  end
+
+puts author.profile
+#=> #<Profile @username="ljenk">
+
+puts profile.author
